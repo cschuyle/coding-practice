e7d3a2897adb17d6a51e79bb34b6dbff0b7c4136
Carlton Schuyler <carl@dragnon.com>
2022-02-01 16:18:10 -0700

    OOified TwoSum a little

--- twosum-java/src/com/dragnon/Main.java
+++ twosum-java/src/com/dragnon/Main.java
@@ -8,35 +8,53 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {2, 7, 11, 15};
-        twosum(arr, 22);
-        twosum_efficient(arr, 22);
-        twosum_space_efficient(arr, 22);
+        int target = 22;
+        run(new NaiveTwoSum(), arr, target);
+        run(new EfficientTwoSum(), arr, target);
+        run(new SpaceEfficientTwoSum(), arr, target);
     }
 
-    static void twosum(int[] arr, int target) {
-        for (int i = 0; i < arr.length; ++i) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] + arr[j] == target) {
-                    outputAnswer(arr, i, j);
-                    return;
+    private static void run(TwoSum twoSum, int[] arr, int target) {
+        var solution = twoSum.apply(arr, target);
+        if (solution != null) {
+            System.out.println(twoSum.getClass().getSimpleName() + " solution to " + Arrays.toString(arr) + " is " + Arrays.toString(solution));
+        } else {
+            System.out.println(twoSum.getClass().getSimpleName() + " solution to " + Arrays.toString(arr) + " does not exist");
+        }
+    }
+}
+
+interface TwoSum {
+    int[] apply(int[] input, int target);
+}
+
+class NaiveTwoSum implements TwoSum {
+    @Override
+    public int[] apply(int[] input, int target) {
+        for (int i = 0; i < input.length; ++i) {
+            for (int j = i + 1; j < input.length; j++) {
+                if (input[i] + input[j] == target) {
+                    return new int[]{i, j};
                 }
             }
         }
+        return null;
     }
+}
 
-    private static void outputAnswer(int[] arr, int i, int j) {
-        System.out.println("Input " + Arrays.toString(arr) + ", output: [" + i + ", " + j + "]");
-    }
+class EfficientTwoSum implements TwoSum {
+
+    @Override
+    public int[] apply(int[] input, int target) {
 
     /*
     O(n):
     Build a hash table mapping values to indices.
     For each value, see if the value that would sum to target exists.
      */
-    static void twosum_efficient(int[] arr, int target) {
         var value_index = new HashMap<Integer, Integer>();
-        for (int i = 0; i < arr.length; ++i) {
-            value_index.put(arr[i], i);
+        for (int i = 0; i < input.length; ++i) {
+            value_index.put(input[i], i);
         }
         for (Map.Entry<Integer, Integer> entry : value_index.entrySet()) {
             int thisValue = entry.getKey();
@@ -44,29 +62,33 @@ public class Main {
             if (value_index.containsKey(target - thisValue)) {
                 int thisIndex = entry.getValue();
                 int otherIndex = value_index.get(otherValue);
-                outputAnswer(arr, thisIndex, otherIndex);
-                return;
+                return new int[]{thisIndex, otherIndex};
             }
         }
+        return null;
     }
-    static short[] value_index = new short[1000000000];
-    static short NO_VALUE = -1;
-    static {
-        for (int i = 0; i < value_index.length; ++i) {
-            value_index[i] = NO_VALUE;
-        }
+}
 
+class SpaceEfficientTwoSum implements TwoSum {
+
+    short[] value_index = new short[1000000000];
+    short NO_VALUE = -1;
+
+    public SpaceEfficientTwoSum() {
+        Arrays.fill(value_index, NO_VALUE);
     }
-    static void twosum_space_efficient(int[] arr, int target) {
 
-        for (int i = 0; i < arr.length; ++i) {
-            value_index[arr[i]] = (short)i;
+    @Override
+    public int[] apply(int[] input, int target) {
+        for (int i = 0; i < input.length; ++i) {
+            value_index[input[i]] = (short) i;
         }
         for (int i = 0; i < value_index.length; ++i) {
             if (value_index[i] != NO_VALUE && value_index[target - i] != NO_VALUE) {
-                outputAnswer(arr, value_index[i], value_index[target - i]);
-                return;
+                return new int[]{value_index[i], value_index[target - i]};
             }
         }
+        return null;
     }
 }
+

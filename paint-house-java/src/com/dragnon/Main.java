f513fc609bddadf6baef6a11bde065bc20ef5c16
Carlton Schuyler <carl@dragnon.com>
2022-02-09 14:09:37 -0700

    Paint House recursive memoized clobbering input because, why not

--- paint-house-java/src/com/dragnon/Main.java
+++ paint-house-java/src/com/dragnon/Main.java
@@ -9,7 +9,7 @@ import static com.dragnon.Color.*;
 public class Main {
 
     public static void main(String[] args) {
-        Solution[] solutions = {new RecursiveMemoizationSolution()};
+        Solution[] solutions = {new RecursiveMemoizationSolution(), new DynamicProgrammingSolution()};
         for (Solution solution : solutions) {
             System.out.println("Solution is " +
                     solution.minCost(new int[][]{new int[]{17, 2, 17}, new int[]{16, 16, 5}, new int[]{14, 3, 19}})); // 10
@@ -88,5 +88,34 @@ class RecursiveMemoizationSolution implements Solution {
         };
         partialSolutions.put(key, partialSolution);
         return partialSolution;
+    }
+}
+
+//class DynamicProgrammingSolution implements Solution {
+//
+//    @Override
+//    public int minCost(int[][] costs) {
+//        int[][] minCosts = new int[costs.length][costs[0].length];
+//        minCosts[0] = costs[0];
+//        for (int i = 1; i < minCosts.length; ++i) {
+//            minCosts[i][RED.value] = costs[i][RED.value] + Math.min(minCosts[i - 1][GREEN.value], minCosts[i - 1][BLUE.value]);
+//            minCosts[i][GREEN.value] = costs[i][GREEN.value] + Math.min(minCosts[i - 1][RED.value], minCosts[i - 1][BLUE.value]);
+//            minCosts[i][BLUE.value] = costs[i][BLUE.value] + Math.min(minCosts[i - 1][RED.value], minCosts[i - 1][GREEN.value]);
+//        }
+//        return Math.min(minCosts[costs.length - 1][RED.value], Math.min(minCosts[costs.length - 1][GREEN.value], minCosts[costs.length - 1][BLUE.value]));
+//    }
+//}
+
+// Or, just don't copy the array
+class DynamicProgrammingSolution implements Solution {
+
+    @Override
+    public int minCost(int[][] costs) {
+        for (int i = 1; i < costs.length; ++i) {
+            costs[i][RED.value] = costs[i][RED.value] + Math.min(costs[i - 1][GREEN.value], costs[i - 1][BLUE.value]);
+            costs[i][GREEN.value] = costs[i][GREEN.value] + Math.min(costs[i - 1][RED.value], costs[i - 1][BLUE.value]);
+            costs[i][BLUE.value] = costs[i][BLUE.value] + Math.min(costs[i - 1][RED.value], costs[i - 1][GREEN.value]);
+        }
+        return Math.min(costs[costs.length - 1][RED.value], Math.min(costs[costs.length - 1][GREEN.value], costs[costs.length - 1][BLUE.value]));
     }
 }
